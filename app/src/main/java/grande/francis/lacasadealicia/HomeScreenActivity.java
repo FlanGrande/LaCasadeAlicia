554364f56575f0fd4b4b4bf1340a10ec066ca981
FrancisGrande <francisjgrande@gmail.com>
2016-05-29 02:14:06 +0100

    Added ToolBar behavior

--- app/src/main/java/grande/francis/lacasadealicia/HomeScreenActivity.java
+++ app/src/main/java/grande/francis/lacasadealicia/HomeScreenActivity.java
@@ -1,7 +1,10 @@
 package grande.francis.lacasadealicia;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeScreenActivity extends AppCompatActivity
 {
@@ -10,5 +13,42 @@ public class HomeScreenActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_screen);
+		Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(myToolbar);
+
+		if(getSupportActionBar() != null)
+		{
+			getSupportActionBar().setLogo(R.drawable.ic_launcher);
+			getSupportActionBar().setDisplayShowTitleEnabled(false);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch(item.getItemId())
+		{
+			case R.id.action_search:
+				// User chose the "Settings" item, show the app settings UI...
+				return true;
+
+			case R.id.action_cart:
+				// User chose the "Favorite" action, mark the current item
+				// as a favorite...
+				return true;
+
+			default:
+				// If we got here, the user's action was not recognized.
+				// Invoke the superclass to handle it.
+				return super.onOptionsItemSelected(item);
+		}
 	}
 }
